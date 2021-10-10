5aead8e678da3c8a968e835629e61bbbe313f4fd
Bacontsu <62763382+bacontsu@users.noreply.github.com>
2021-10-11 02:30:58 +0700

    Add files via upload

---
+++ FlappybirdWinForms/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace FlappybirdWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bird = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gameover = new System.Windows.Forms.Label();
+            this.wallBot = new System.Windows.Forms.Panel();
+            this.wallUp = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // bird
+            // 
+            this.bird.BackgroundImage = global::FlappybirdWinForms.Properties.Resources.toppng_com_flappy_bird_sprite_315x223;
+            this.bird.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bird.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bird.Enabled = false;
+            this.bird.FlatAppearance.BorderSize = 0;
+            this.bird.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bird.Location = new System.Drawing.Point(270, 53);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(80, 80);
+            this.bird.TabIndex = 0;
+            this.bird.UseVisualStyleBackColor = true;
+            this.bird.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // gameover
+            // 
+            this.gameover.AutoSize = true;
+            this.gameover.BackColor = System.Drawing.Color.Black;
+            this.gameover.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameover.ForeColor = System.Drawing.Color.White;
+            this.gameover.Location = new System.Drawing.Point(148, 173);
+            this.gameover.Name = "gameover";
+            this.gameover.Size = new System.Drawing.Size(323, 55);
+            this.gameover.TabIndex = 1;
+            this.gameover.Text = "GAME OVER";
+            this.gameover.MouseDown += new System.Windows.Forms.MouseEventHandler(this.gameover_MouseDown);
+            // 
+            // wallBot
+            // 
+            this.wallBot.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.wallBot.BackColor = System.Drawing.Color.ForestGreen;
+            this.wallBot.Location = new System.Drawing.Point(524, 283);
+            this.wallBot.Name = "wallBot";
+            this.wallBot.Size = new System.Drawing.Size(88, 146);
+            this.wallBot.TabIndex = 2;
+            // 
+            // wallUp
+            // 
+            this.wallUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.wallUp.BackColor = System.Drawing.Color.ForestGreen;
+            this.wallUp.Location = new System.Drawing.Point(524, 12);
+            this.wallUp.Name = "wallUp";
+            this.wallUp.Size = new System.Drawing.Size(88, 146);
+            this.wallUp.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panel1.Location = new System.Drawing.Point(94, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(430, 38);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panel3.Location = new System.Drawing.Point(524, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(100, 442);
+            this.panel3.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panel4.Location = new System.Drawing.Point(-1, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(100, 442);
+            this.panel4.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panel2.Location = new System.Drawing.Point(99, 404);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(430, 38);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.gameover);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.wallUp);
+            this.Controls.Add(this.wallBot);
+            this.Controls.Add(this.bird);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bird;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label gameover;
+        private System.Windows.Forms.Panel wallBot;
+        private System.Windows.Forms.Panel wallUp;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ FlappybirdWinForms/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappybirdWinForms
+{
+    public partial class Form1 : Form
+    {
+        bool isJumping = false;
+        bool lose = false;
+        bool generationSeed = false;
+        public int seed;
+        public int seedOld;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bird.Top = 150;
+            bird.Left = 270;
+            gameover.Hide();
+            this.MaximumSize = new Size(640, 480);
+            this.MinimumSize = new Size(640, 480);
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!lose)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    timer1.Start();
+                    isJumping = true;
+
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //move the obstacles
+            if(!lose)
+            {
+                wallBot.Left -=2 ;
+                wallUp.Left -= 2;
+            }
+
+
+
+            //jumping handler
+            if(isJumping)
+            {   if(bird.Top !=0)
+                bird.Top -= 2;
+            }
+            else
+            {
+                bird.Top += 2;
+            }
+
+            if(bird.Top > 367)
+            {
+                GameOver();
+            }
+
+            //teleport the obstacles and generate new shape
+            if(wallBot.Left < 12 && wallUp.Left < 12)
+            {
+                wallBot.Left = 524;
+                wallUp.Left = 524;
+
+                int seed = new Random().Next(-100, 100);
+
+                if (wallUp.Height - seed >= 50 && wallBot.Height + seed >= 50)
+                {
+                    wallUp.Height -= seed;
+                    wallBot.Top -= seed;
+                    wallBot.Height += seed;
+                }
+
+
+
+
+            }
+
+            //collision handler
+            if(184 <= wallBot.Left && wallBot.Left <= 350)
+            {
+                if ((bird.Top+80) >= (wallBot.Top))
+                {
+                    GameOver();
+                }
+            }
+
+            if (184 <= wallUp.Left && wallUp.Left <= 350)
+            {
+                if((bird.Top) <= (12+wallUp.Height))
+                {
+                    GameOver();
+                }
+            }
+
+
+
+
+        }
+
+
+        void GameOver()
+        {
+            timer1.Stop();
+            lose = true;
+            gameover.Show();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            isJumping = false;
+        }
+
+        private void gameover_MouseDown(object sender, MouseEventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
